a9e2ceb27b6eb9c31d82285190db078048a6864f
OMI786I <sohrawardy1998@gmail.com>
2025-02-08 03:38:57 +0600

    added drawer section

--- components/DrawerContent.tsx
+++ components/DrawerContent.tsx
@@ -1,14 +1,110 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import React, { ReactNode } from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { LinearGradient } from "expo-linear-gradient";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+interface MenuItem {
+  title: string;
+  icons: ReactNode;
+  link: string;
+}
+
+const data: MenuItem[] = [
+  {
+    title: "Orders",
+    icons: <Ionicons name="basket-outline" size={28} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Wishlist",
+    icons: <AntDesign name="hearto" size={28} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Delivery Address",
+    icons: <Ionicons name="map" size={28} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Payment Method",
+    icons: <AntDesign name="creditcard" size={24} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Promo",
+    icons: <AntDesign name="carryout" size={24} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Notifications",
+    icons: <AntDesign name="bells" size={24} color="white" />,
+    link: "/",
+  },
+  {
+    title: "Help",
+    icons: <AntDesign name="question" size={24} color="white" />,
+    link: "/",
+  },
+  {
+    title: "About",
+    icons: <AntDesign name="exclamation" size={24} color="white" />,
+    link: "/",
+  },
+];
 
 const DrawerContent = ({ navigation }: any) => {
   return (
-    <View className="flex-1 p-4">
-      <Text className="text-lg font-bold mb-4">Settings Menu</Text>
-      <TouchableOpacity onPress={() => navigation.closeDrawer()}>
-        <Text>Close Drawer</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <LinearGradient
+        colors={["#7F77FE", "#A573FF"]}
+        style={{ flex: 1, minHeight: "100%", padding: 30 }}
+      >
+        {/* User Profile Section */}
+        <View className="flex-row items-center mb-6">
+          <Image
+            className="h-16 w-16 rounded-full mr-4"
+            source={{ uri: "https://i.ibb.co/nqpJrWtt/avatar.jpg" }}
+          />
+
+          <View>
+            <View className="flex-row items-center gap-1">
+              <Text className="text-xl font-bold text-white">
+                Tahid Sohrawardy Omi
+              </Text>
+              <TouchableOpacity>
+                <AntDesign name="edit" size={20} color="white" />
+              </TouchableOpacity>
+            </View>
+            <Text className="text-gray-200">sohrawardy1998@gmail.com</Text>
+          </View>
+        </View>
+
+        {/* Menu Items */}
+
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            return (
+              <View className=" my-2 ">
+                <TouchableOpacity className="flex-row gap-2">
+                  {item.icons}
+
+                  <Text className="text-white"> {item.title}</Text>
+                </TouchableOpacity>
+              </View>
+            );
+          }}
+        />
+      </LinearGradient>
+    </ScrollView>
   );
 };
 
